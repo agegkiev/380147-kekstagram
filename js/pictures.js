6427c495fbde1c8dcdeaaa1cd388f280d55d83b7
agegkiev <gegkiev@bk.ru>
2017-12-04 01:23:12 +0300

    fix

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var UserPhotosArray = [];
+var arrShuffle = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var container = document.querySelector('.pictures');
 var template = document.querySelector('#picture-template').content;
@@ -8,6 +9,11 @@ var total = 26;
 var img = template.querySelector('img');
 var comments = template.querySelector('.picture-comments');
 var likes = template.querySelector('.picture-likes');
+var fragment = document.createDocumentFragment();
+var gallery = document.querySelector('.gallery-overlay');
+var galleryImage = gallery.querySelector('.gallery-overlay-image');
+var galleryLikes = gallery.querySelector('.likes-count');
+var galleryComments = gallery.querySelector('.comments-count');
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -17,10 +23,22 @@ function getRandomElement(arr) {
   return arr[getRandomInRange(0, arr.length)];
 }
 
+function shuffle(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var rand = Math.floor(Math.random() * (i + 1));
+    var temp = arr[i];
+    arr[i] = arr[rand];
+    arr[rand] = temp;
+  }
+  return arr;
+}
+
+arrShuffle = shuffle(arrShuffle);
+
 function createUserPhotosArray(total) {
   for (var i = 1; i <= total; i++) {
     UserPhotosArray[i] = {
-      url: 'photos/' + i + '.jpg',
+      url: 'photos/' + arrShuffle[i] + '.jpg',
       likes: getRandomInRange(25, 200),
       comments: getRandomElement(COMMENTS)
     };
@@ -28,17 +46,15 @@ function createUserPhotosArray(total) {
   return UserPhotosArray;
 }
 
-var UserPhotosArray = createUserPhotosArray(total);
+UserPhotosArray = createUserPhotosArray(total);
 
 function createDomElement(numb) {
   img.setAttribute('src', UserPhotosArray[numb].url);
-  comments.textContent = UserPhotosArray[numb].comments;
+  comments.textContent = UserPhotosArray[numb].length;
   likes.textContent = UserPhotosArray[numb].likes;
   var element = template.cloneNode(true);
   return element;
 }
-
-var fragment = document.createDocumentFragment();
 
 for (var i = 1; i <= total; i++) {
   fragment.appendChild(createDomElement(i));
@@ -46,16 +62,11 @@ for (var i = 1; i <= total; i++) {
 
 container.appendChild(fragment);
 
-var gallery = document.querySelector('.gallery-overlay');
 gallery.classList.remove('hidden');
-
-var galleryImage = gallery.querySelector('.gallery-overlay-image');
-var galleryLikes = gallery.querySelector('.likes-count');
-var galleryComments = gallery.querySelector('.comments-count');
 
 function createGalleryOverlay(numb) {
   galleryImage.setAttribute('src', UserPhotosArray[numb].url);
-  galleryComments.textContent = UserPhotosArray[numb].comments;
+  galleryComments.textContent = getRandomInRange(1, 2);
   galleryLikes.textContent = UserPhotosArray[numb].likes;
   var element = template.cloneNode(true);
   return element;
